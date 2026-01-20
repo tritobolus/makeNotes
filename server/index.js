fab0c6ced21fd700b2b3beeb16d9b104adc5f7ce
akashhowly <yourhowlyno1@gmail.com>
2026-01-20 17:15:57 +0530

    fix bug

--- server/index.js
+++ server/index.js
@@ -13,10 +13,14 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://make-notes-theta.vercel.app"
+  ],
+  credentials: true
+}));
 
 connectDB();
 
